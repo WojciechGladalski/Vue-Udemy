a3c577168d284c44df68349b83fbab1580103353
Wojciech <wojciech.gladalski@gmail.com>
2020-03-15 17:32:47 +0100

    dynamiczne komponenty są niszczone

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase counter!</button>
+        <p>{{counter}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "New"
+        name: "New",
+        data: function () {
+            return {
+                counter: 0
+            }
+        },
+        destroyed() {
+            console.log('Destoyed!')
+        }
     }
 </script>
 
