cbf67e018633a1441f8fc5c500a8374cdcfd92d6
Wojciech <wojciech.gladalski@gmail.com>
2020-03-15 16:22:17 +0100

    simple passing property from parent to child

--- src/deprecatedComponents/app3/App.vue
+++ src/deprecatedComponents/app3/App.vue
@@ -2,22 +2,25 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote quote="'A wonderfull quote!'"></app-quote>
+                <app-user></app-user>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from "@/components/Quote";
+    import User from './components/User.vue';
 
     export default {
         components: {
-            appQuote: Quote
+            appUser: User
         }
     }
 </script>
 
 <style>
-
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
--- src/deprecatedComponents/app3/main.js
+++ src/deprecatedComponents/app3/main.js
@@ -8,6 +8,14 @@ Vue.config.productionTip = false
 //Vue.component('app-servers', Home)
 //globalny komponent
 
+export const eventBus = new Vue({
+    methods: {
+        changeAge(age) {
+            this.$emit('ageWasEdited', age)
+        }
+    }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
