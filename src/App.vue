3ab913bcfca1bbfbba5d1a4cf109d36c41a08197
Wojciech <wojciech.gladalski@gmail.com>
2020-03-15 17:27:40 +0100

    dynamiczne przełączanie pomiędzy komponentami

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote>
-                    <h2 slot="title">{{quoteTitle}}</h2>
-                    <p>'This is a wonderfull quote!'</p>
-                </app-quote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+                <hr>
+                <component :is="selectedComponent">
+                    Default content
+                </component>
+<!--                <app-quote>-->
+<!--                    <h2 slot="title">{{quoteTitle}}</h2>-->
+<!--                    <p>'This is a wonderfull quote!'</p>-->
+<!--                </app-quote>-->
             </div>
         </div>
     </div>
@@ -13,21 +22,26 @@
 
 <script>
     import Quote from "@/components/Quote";
+    import Author from "@/components/Author";
+    import New from "@/components/New";
 
     export default {
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            // eslint-disable-next-line vue/no-unused-components
+            appAuthor: Author,
+            // eslint-disable-next-line vue/no-unused-components
+            appNew: New
         },
         data: function () {
             return {
-                quoteTitle: 'A Quote'
+                quoteTitle: 'A Quote',
+                selectedComponent: 'appQuote'
             }
         }
     }
 </script>
 
 <style>
-    h2 {
-        color: red;
-    }
+
 </style>
