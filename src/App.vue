308efdbee61c9641032a42df9494001303ed62a6
Wojciech <wojciech.gladalski@gmail.com>
2020-03-19 20:39:04 +0100

    przygotowanie pod sekcje 11

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,126 @@
 <template>
     <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control">
+                        <option></option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
         <div class="row">
-            <div class="col-xs-12">
-                <button @click="selectedComponent = 'appQuote'">Quote</button>
-                <button @click="selectedComponent = 'appAuthor'">Author</button>
-                <button @click="selectedComponent = 'appNew'">New</button>
-                <hr>
-                <p>{{selectedComponent}}</p>
-                <hr>
-                <keep-alive>
-                    <component :is="selectedComponent">
-                        Default content
-                    </component>
-                </keep-alive>
-<!--                <app-quote>-->
-<!--                    <h2 slot="title">{{quoteTitle}}</h2>-->
-<!--                    <p>'This is a wonderfull quote!'</p>-->
-<!--                </app-quote>-->
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail:</p>
+                        <p>Password:</p>
+                        <p>Age:</p>
+                        <p>Message: </p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                        <p>Gender:</p>
+                        <p>Priority:</p>
+                        <p>Switched:</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from "@/components/Quote";
-    import Author from "@/components/Author";
-    import New from "@/components/New";
-
     export default {
-        components: {
-            appQuote: Quote,
-            // eslint-disable-next-line vue/no-unused-components
-            appAuthor: Author,
-            // eslint-disable-next-line vue/no-unused-components
-            appNew: New
-        },
-        data: function () {
-            return {
-                quoteTitle: 'A Quote',
-                selectedComponent: 'appQuote'
-            }
-        }
     }
 </script>
 
---
+++ src/deprecatedComponents/app4/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        Default content
+                    </component>
+                </keep-alive>
+<!--                <app-quote>-->
+<!--                    <h2 slot="title">{{quoteTitle}}</h2>-->
+<!--                    <p>'This is a wonderfull quote!'</p>-->
+<!--                </app-quote>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from "@/components/Quote";
+    import Author from "@/components/Author";
+    import New from "@/components/New";
+
+    export default {
+        components: {
+            appQuote: Quote,
+            // eslint-disable-next-line vue/no-unused-components
+            appAuthor: Author,
+            // eslint-disable-next-line vue/no-unused-components
+            appNew: New
+        },
+        data: function () {
+            return {
+                quoteTitle: 'A Quote',
+                selectedComponent: 'appQuote'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
