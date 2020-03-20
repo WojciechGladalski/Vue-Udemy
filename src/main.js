f7a16f8d2d68e7a136ef65bd9ee1d4471cab0986
Wojciech <wojciech.gladalski@gmail.com>
2020-03-20 13:40:27 +0100

    modyfikatory argumentow dyrektyw

--- src/main.js
+++ src/main.js
@@ -15,9 +15,15 @@ Vue.directive('highlight', {
     bind(el, binding, vnode) {
         //el.style.backgroundColor = 'green'
         //el.style.backgroundColor = binding.value
-        if (binding.arg == 'background') {
-            el.style.backgroundColor = binding.value
+        var delay = 0;
+        if (binding.modifiers['delayed']) {
+            delay = 3000
         }
+        setTimeout(()=> {
+            if (binding.arg == 'background') {
+                el.style.backgroundColor = binding.value
+            }
+        }, delay)
     }
 })
 
