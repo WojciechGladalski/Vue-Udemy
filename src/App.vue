10c1d87535d709a714abf822c115937a540c1e86
Wojciech <wojciech.gladalski@gmail.com>
2020-03-20 13:47:42 +0100

    modyfikatory argumentow dyrektyw BUT lokalnie

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-text-highlight:color.delayed="'blue'">Color but text only</p>
             </div>
         </div>
     </div>
@@ -18,6 +19,22 @@
 
 <script>
     export default {
+        directives: {
+            'local-text-highlight': {
+                // eslint-disable-next-line no-unused-vars
+                bind(el, binding, vnode) {
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                        delay = 3000
+                    }
+                    setTimeout(()=> {
+                        if (binding.arg == 'color') {
+                            el.style.color = binding.value
+                        }
+                    }, delay)
+                }
+            }
+        }
     }
 </script>
 
