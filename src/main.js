6b573510a3b66ed1f7fd7edd1e1a2888a05d8336
Wojciech <wojciech.gladalski@gmail.com>
2020-03-20 13:23:57 +0100

    dyrektywy wbudowane i dyrektywy customowe

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ Vue.config.productionTip = false
 //Vue.component('app-servers', Home)
 //globalny komponent
 
+//customowe dyrektywy
+Vue.directive('highlight', {
+    //są różne metody podczas ustawianie dyrektyw (mają różny moment generowania się)
+    // eslint-disable-next-line no-unused-vars
+    bind(el, binding, vnode) {
+        el.style.backgroundColor = 'green'
+    }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
