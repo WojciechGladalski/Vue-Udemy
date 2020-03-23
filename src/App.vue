fad2156533174631110031d8d9382ee69369b1d0
Wojciech <wojciech.gladalski@gmail.com>
2020-03-23 11:34:29 +0100

    animation and transition razem

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
                 <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
-                <transition name="slide">
+                <transition name="slide" type="animation">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
@@ -43,15 +43,19 @@
     }
     .slide-enter {
         /*transform: translateY(20px);*/
+        opacity: 0;
     }
     .slide-enter-active {
         animation: slide-in 1s ease-out forwards;
+        transition: opacity 0.5s;
     }
     slide-leave {
 
     }
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
+        transition: opacity 3s;
+        opacity: 0;
     }
     @keyframes slide-in {
         from {
