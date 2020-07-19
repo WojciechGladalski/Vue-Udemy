cd4d6a9c2569729ea57596f248501db81c1544af
Wojciech <wojciech.gladalski@gmail.com>
2020-07-19 18:03:18 +0200

    functioning aria tabs from w3.org

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <Css/>
+<!--        <Css/>-->
+        <AriaTabs/>
     </div>
 </template>
 
 <script>
     import Css from "@/components/Css";
+    import AriaTabs from "@/components/AriaTabs";
     export default {
         components: {
+            AriaTabs,
+            // eslint-disable-next-line vue/no-unused-components
             Css
         }
     }
